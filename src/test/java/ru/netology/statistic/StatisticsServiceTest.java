334c3fd921f3ece7a7a0395a52217d8216b2ba11
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-01-15 18:44:35 +0300

    add test midle. add test end. add plugun 100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -28,4 +28,26 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void findMaxBeginning() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {90, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 90;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 20, 12};
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
 }
